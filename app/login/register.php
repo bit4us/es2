96fa919b30e46ce353c79b5e7a697d8053113ccb
bit4us <bit4us@gmail.com>
2022-11-18 15:25:49 +0100

    asdf

--- app/login/register.php
+++ app/login/register.php
@@ -10,23 +10,23 @@ if($_POST['fname'] && $_POST['lname'] && $_POST['email'] && $_POST['password'] &
     header('Location: ./login.php?r=pc');  //password confirmation error
     exit;
   }
-  $user = new User();
-  $user->setFirstName($_POST['fname']);
-  $user->setLastName($_POST['lname']);
-  $user->setEmail($_POST["email"]);
-  $user->setPassword(md5($_POST['password']));
-
-  $registered = $user->registerUser();
-  // echo "<br>registration message: ".$registrationMessage;
-  if($registered){
+  try{
+    $user = new User();
+    $user->setFirstName($_POST['fname']);
+    $user->setLastName($_POST['lname']);
+    $user->setEmail($_POST["email"]);
+    $user->setPassword(md5($_POST['password']));
+    $registered = $user->registerUser();
+    echo "<br>registration message: ".$registrationMessage;
     $user->authenticate();
     $_SESSION['id'] = $user->getID();
-    header("Location: ../dashboard"); 
-    exit;
+    // header("Location: ../dashboard"); 
+    // exit;
   }
-  else{
-    header("Location: ./index.php/?r=rf"); //registration failed
-    exit;
+  catch(Exception $e){
+    echo 'Message: ' .$e->getMessage();
+    //header("Location: ./index.php/?r=rf"); //registration failed
+    // exit;
   }
 }
 else{
